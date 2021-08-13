f8bb222924e6a5ca0b8a9da8989ce42083b81c6a
maxloosmu <maxloo@smu.edu.sg>
2021-08-13 22:12:05 +0800

    updated drools6-dev-guide/chapter-02/

--- drools6-dev-guide/chapter-02/myfirst-drools-project/src/main/java/org/drools/devguide/App.java
+++ drools6-dev-guide/chapter-02/myfirst-drools-project/src/main/java/org/drools/devguide/App.java
@@ -12,6 +12,12 @@
 // }
 
 package org.drools6.book;
+// import org.drools.devguide.eshop.model;
+// package org.drools.devguide;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.drools.devguide.eshop.model.Item;
 
 public class App {
   public static void main( String[] args ) {
@@ -20,13 +26,17 @@ public class App {
     KieServices ks = KieServices.Factory.get();
     KieContainer kContainer = ks.getKieClasspathContainer();
     KieSession kSession = kContainer.newKieSession();
-    Item item = new Item("A", 123.0,234.0);
+    Item item = new Item("A", 234.0, 0.0);
+    Item item0 = new Item("B", 123.0, 0.0);
     System.out.println( "Item Category: " + item.getCategory());
+    System.out.println( "Item Category: " + item0.getCategory());
     //2) Provide information to the Rule Engine Context
     kSession.insert(item);
+    kSession.insert(item0);
     //3) Execute the rules that are matching
     int fired = kSession.fireAllRules();
     System.out.println( "Number of Rules executed = " + fired );
     System.out.println( "Item Category: " + item.getCategory());
+    System.out.println( "Item Category: " + item0.getCategory());
   }
 }
